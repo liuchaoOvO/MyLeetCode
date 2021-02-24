6d2d65194dd6d0ecdf300b76197e3bdeeffcb0ae
liuchao02@idstaff.com <liuchao02@idstaff.com>
2021-02-24 16:57:54 +0800

    【javabase】20210224 两个stack实现一个队列的demo

--- leetcode/src/com/lc/javabase/MyQueue.java
+++ leetcode/src/com/lc/javabase/MyQueue.java
@@ -12,46 +12,33 @@ import java.util.Stack;
  * @date 2021/2/24 16:32
  */
 public class MyQueue<E> {
-        //用的jdk自带的栈
-        private Stack<E> s1=new Stack<>();
-        private Stack<E> s2=new Stack<>();
-        public void offer(E val){   //入队
-            s1.push(val);
-        }
-        public E poll() {   //出队
-            while (s2.empty()){
-                while (!s1.empty()){
-                    s2.push(s1.peek());
-                    s1.pop();
-                }
-            }
-            E val=s2.peek();
-            s2.pop();
-            //获取出队元素后，再将s2里面的元素放入s1里面。
-            while (!s2.empty()){
-                s1.push(s2.pop());
-            }
-            return val;
-        }
+    //用的jdk自带的栈
+    private Stack<E> s1 = new Stack<>();
+    private Stack<E> s2 = new Stack<>();
 
-        public E peek(){//查看对头元素
-            while (s2.empty()){
-                while (!s1.empty()){
-                    s2.push(s1.peek());
-                    s1.pop();
-                }
-            }
-            E val=s2.peek();
-            //获取出队元素后，再将s2里面的元素放入s1里面。
-            while (!s2.empty()){
-                s1.push(s2.pop());
-            }
-            return val;
-        }
+    public void offer(E val) {   //入队
+        s1.push(val);
+    }
 
-        public boolean empty(){ //判断队是否为空
-            return s1.empty();
+    public E poll() {   //出队
+        while (s2.empty()) {
+            while (!s1.empty()) {
+                s2.push(s1.pop());
+
+            }
         }
+        E val = s2.pop();
+        //获取出队元素后，再将s2里面的元素放入s1里面。
+        while (!s2.empty()) {
+            s1.push(s2.pop());
+        }
+        return val;
+    }
+
+
+    public boolean empty() { //判断队是否为空
+        return s1.empty();
+    }
 
     //测试
     public static void main(String[] args) {
@@ -65,7 +52,7 @@ public class MyQueue<E> {
         System.out.println();
         System.out.println("出队：");
         while (!queue.empty()) {
-            System.out.print(queue.poll()+" ");
+            System.out.print(queue.poll() + " ");
         }
     }
 }
